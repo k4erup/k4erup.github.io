27419d6fa254ab88c7784936f5fc2b50be3d4073
かえる <k4erup@gmail.com>
2025-01-23 15:09:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-window.onload = function() {
+window.addEventListener('DOMContentLoaded', () => {
     const birthDate = new Date('2003-09-11');
     const today = new Date();
     let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDifference = today.getMonth() - birthDate.getMonth();
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
+    const isBeforeBirthday = today.getMonth() < birthDate.getMonth() || 
+        (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate());
+    if (isBeforeBirthday) age--;
     document.getElementById('age').textContent = `${age}歳`;
-};
+});
