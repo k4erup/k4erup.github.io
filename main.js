b286aebd127050ae9704275deeaa6f9b21648ee1
かえる <k4erup@gmail.com>
2025-01-23 15:54:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ window.addEventListener('DOMContentLoaded', () => {
         (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate());
     if (isBeforeBirthday) age--;
     document.getElementById('age').textContent = `${age}歳`;
+
+    const parser = new budoux.Parser();
+    const paragraphs = document.querySelectorAll('p'); // すべての<p>要素に適用
+
+    paragraphs.forEach((paragraph) => {
+        const originalText = paragraph.textContent;
+        const processedHTML = parser.translateHTMLString(`<span>${originalText}</span>`);
+        paragraph.innerHTML = processedHTML;
+    });
 });
